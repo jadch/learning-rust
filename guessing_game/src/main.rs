e697c0271763f68469d62ec1be66871cc638bdd6
Jad <jad.chalita@gmail.com>
2021-12-17 18:22:53 +0100

    added our first dependency + asked user for input

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,14 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Try guessing the number!");
+    println!("start by inputting your guess....");
+
+    let mut user_guess = String::new();
+
+    io::stdin()
+        .read_line(&mut user_guess)
+        .expect("Failed to read line");
+
+    println!("You guessed {}", user_guess);
 }

e9c8b248bbaf63b69cc34ff77715b52bd6ff2507
Jad <jad.chalita@gmail.com>
2021-12-17 20:38:09 +0100

    feature: now handling potential error returned by parse

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -14,8 +14,16 @@ fn main() {
         io::stdin()
             .read_line(&mut user_guess)
             .expect("Failed to read line");
-        let user_guess: u32 = user_guess.trim().parse().expect("Please input a number !");
+
+        let user_guess: u32 = match user_guess.trim().parse() {
+            Ok(value) => value,
+            Err(_) => {
+                println!("Please input a number !");
+                continue;
+            }
+        };
         println!("You guessed {}", user_guess);
+
         match user_guess.cmp(&secret_num) {
             Ordering::Less => println!("secret number is higher than {}. Try again", user_guess),
             Ordering::Greater => println!("secret number is lower than {}. Try again", user_guess),

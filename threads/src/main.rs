cc04e1c885d85858df0ea2d198a2c3f12827d31b
Jad <jad.chalita@gmail.com>
2022-07-23 16:48:13 +0200

    moving ownership of a variable to a thread

--- threads/src/main.rs
+++ threads/src/main.rs
@@ -1,23 +1,37 @@
-use std::{thread, time::Duration};
+// use std::{thread, time::Duration};
+use std::thread;
 
 fn main() {
-    let handle = thread::spawn(|| {
-        for i in 1..11 {
-            println!(
-                "spawned thread > is logging number {} of 10 and sleeping 1ms",
-                i
-            );
-            thread::sleep(Duration::from_millis(1))
-        }
-    });
+    let v = vec![1, 2, 3];
 
-    for i in 1..6 {
+    let handle = thread::spawn(move || {
         println!(
-            "main thread > is logging number {} of 5 and sleeping 1ms",
-            i
+            "spawned thread > spawned thread now has ownership of vector {:?}",
+            v
         );
-        thread::sleep(Duration::from_millis(1))
-    }
+    });
 
     handle.join().unwrap();
 }
+
+// fn main() {
+//     let handle = thread::spawn(|| {
+//         for i in 1..11 {
+//             println!(
+//                 "spawned thread > is logging number {} of 10 and sleeping 1ms",
+//                 i
+//             );
+//             thread::sleep(Duration::from_millis(1))
+//         }
+//     });
+
+//     for i in 1..6 {
+//         println!(
+//             "main thread > is logging number {} of 5 and sleeping 1ms",
+//             i
+//         );
+//         thread::sleep(Duration::from_millis(1))
+//     }
+
+//     handle.join().unwrap();
+// }

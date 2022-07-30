d729cc068e0f03d4bd9bec4ff817e7dd1f8463ee
Jad <jad.chalita@gmail.com>
2022-07-30 11:28:51 +0200

    WIP - adding functions to enum

--- rust_book_follow_along/enums/src/main.rs
+++ rust_book_follow_along/enums/src/main.rs
@@ -8,6 +8,24 @@ struct IpAddress {
     address: String,
 }
 
+// enum Option<T> {
+//     Some(T),
+//     None,
+// }
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+impl Message {
+    fn some_function() {
+        println!("Hey this is the Message.some_function()")
+    }
+}
+
 fn main() {
     let localhost: IpAddressKind = IpAddressKind::V4(127, 0, 0, 1);
 }

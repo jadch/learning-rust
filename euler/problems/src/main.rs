67e50eedc2c1907c2a9576c23967e4a8bb9f86b1
Jad <jad.chalita@gmail.com>
2023-07-09 13:44:56 +0200

    problem 3

--- euler/problems/src/main.rs
+++ euler/problems/src/main.rs
@@ -1,6 +1,8 @@
 mod problem_3;
 
 fn main() {
-    println!("Hello, world!");
-    problem_3::largest_prime_factor();
+    println!("computing solution...");
+
+    let solution = problem_3::largest_prime_factor(600851475143, 1);
+    println!("solution: {}", solution);
 }
--- euler/problems/src/problem_3.rs
+++ euler/problems/src/problem_3.rs
@@ -2,8 +2,9 @@
 
 /*
 <p>The prime factors of 13195 are 5, 7, 13 and 29.</p>
-<p>What is the largest prime factor of the number N = 600851475143$?</p>
+<p>What is the largest prime factor of the number N = 600851475143 ?</p>
 
+SOLUTION: 6857
 
 ANSWER:
     - go through prime numbers in ASC order
@@ -12,7 +13,34 @@ ANSWER:
         => when the function is called on a number that is prime => return that number,
         the largest prime factor of N
 */
+use num::integer::Roots;
 
-pub fn largest_prime_factor() {
-    println!("Hello, world largest_prime_factor !");
+
+
+pub fn largest_prime_factor(number: i64, largest_prime_factor_var: i64) -> i64 {
+    let mut current_largest_prime_factor_var: i64 = largest_prime_factor_var;
+
+    if number % 2 == 0 {
+        if 2 > current_largest_prime_factor_var {
+            current_largest_prime_factor_var = 2
+        }
+        return largest_prime_factor(number/2, current_largest_prime_factor_var);
+    }
+
+    let mut i: i64 = 3;
+    while i <= number.sqrt() {
+        if number % i == 0 {
+            if i > current_largest_prime_factor_var {
+                current_largest_prime_factor_var = i;
+            }
+            return largest_prime_factor(number/i, current_largest_prime_factor_var);
+        }
+        i = i + 2;
+    }
+    
+    if number > current_largest_prime_factor_var {
+        return number;
+    } else {
+        return current_largest_prime_factor_var;
+    };
 }

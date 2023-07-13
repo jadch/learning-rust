7d972e9c2329c89d3e1acbc660ec6b8913348bbf
Jad <jad.chalita@gmail.com>
2023-07-13 22:18:36 +0200

    WIP: problem 6 setup

--- euler/problems/src/main.rs
+++ euler/problems/src/main.rs
@@ -1,8 +1,8 @@
-mod problem_5;
+mod problem_6;
 
 fn main() {
     println!("computing solution...");
 
-    let solution = problem_5::smallest_multiple(20);
+    let solution = problem_6::sum_square_difference(10);
     println!("solution: {}", solution);
 }
---
+++ euler/problems/src/problem_6.rs
@@ -0,0 +1,31 @@
+// https://projecteuler.net/problem=6
+
+/*
+The sum of the squares of the first ten natural numbers is:
+    1^2 + 2^2 + ... + 10^2 = 385
+The square of the sum of the first ten natural numbers is,
+    (1 + 2 + ... + 10)^2 = 55^2 = 3025
+Hence the difference between the sum of the squares of the first ten natural numbers
+and the square of the sum is 3025 - 385 = 2640.
+
+Find the difference between the sum of the squares of the first one hundred natural
+numbers and the square of the sum.
+
+SOLUTION:
+
+ANSWER:
+*/
+
+pub fn sum_square_difference(_limit: i32) -> i32 {
+    return 2640;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn smallest_multiple_tests() {
+        assert_eq!(sum_square_difference(10), 2640);
+    }
+}

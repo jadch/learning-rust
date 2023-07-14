bde8a76301f7ab945bb4248546fba607c8b90c9c
Jad <jad.chalita@gmail.com>
2023-07-14 10:32:32 +0200

    problem 6

--- euler/problems/src/main.rs
+++ euler/problems/src/main.rs
@@ -3,6 +3,6 @@ mod problem_6;
 fn main() {
     println!("computing solution...");
 
-    let solution = problem_6::sum_square_difference(10);
+    let solution = problem_6::sum_square_difference(100);
     println!("solution: {}", solution);
 }
--- euler/problems/src/problem_6.rs
+++ euler/problems/src/problem_6.rs
@@ -11,13 +11,18 @@ and the square of the sum is 3025 - 385 = 2640.
 Find the difference between the sum of the squares of the first one hundred natural
 numbers and the square of the sum.
 
-SOLUTION:
+SOLUTION: brute force
 
-ANSWER:
+ANSWER: 25164150
 */
 
-pub fn sum_square_difference(_limit: i32) -> i32 {
-    return 2640;
+pub fn sum_square_difference(limit: i64) -> i64 {
+    let numbers: Vec<i64> = (1..=limit).collect();
+
+    let sum_of_squares: i64 = numbers.iter().map(|num| num.pow(2)).sum();
+    let square_of_sum: i64 = numbers.iter().sum::<i64>().pow(2);
+    
+    return square_of_sum - sum_of_squares;
 }
 
 #[cfg(test)]
@@ -27,5 +32,6 @@ mod tests {
     #[test]
     fn smallest_multiple_tests() {
         assert_eq!(sum_square_difference(10), 2640);
+        assert_eq!(sum_square_difference(100), 25164150);
     }
 }

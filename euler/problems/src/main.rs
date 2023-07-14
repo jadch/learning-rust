02e6a81a3d88cd3e762cf58ca2ead756fd8b3241
Jad <jad.chalita@gmail.com>
2023-07-14 21:58:29 +0200

    problem_8: largest_product_in_series

--- euler/problems/src/main.rs
+++ euler/problems/src/main.rs
@@ -3,6 +3,6 @@ mod problem_8;
 fn main() {
     println!("computing solution...");
 
-    let solution = problem_8::largest_product_in_series(4);
+    let solution = problem_8::largest_product_in_series(13);
     println!("solution: {}", solution);
 }

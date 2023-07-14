a6595c22bb23d67d7c49d7d64b75c483ffc439ff
Jad <jad.chalita@gmail.com>
2023-07-14 20:59:56 +0200

    sliced_vec

--- random_tutorials/src/iterators.rs
+++ random_tutorials/src/iterators.rs
@@ -22,10 +22,14 @@ fn iterator_methods() {
     println!("sum: {}", sum); // 10
 
     let summed_vec: Vec<i32> = vec.iter().map(|x| x + 1).collect();
-    println!("summed_vec: {:?}", summed_vec);  // [2, 3, 4, 5]
+    println!("summed_vec: {:?}", summed_vec); // [2, 3, 4, 5]
     println!("vec: {:?}", vec); // [1, 2, 3, 4]
 
     let filtered_vec: Vec<&i32> = vec.iter().filter(|x| x > &&3).collect();
     println!("filtered_vec: {:?}", filtered_vec); // [4]
+    println!("vec: {:?}", vec); // [1, 2, 3, 4]
+
+    let sliced_vec = &vec[1..3].to_vec();
+    println!("sliced_vec: {:?}", sliced_vec); // [2, 3]
     println!("vec: {:?}", vec); // [1, 2, 3, 4]
 }
